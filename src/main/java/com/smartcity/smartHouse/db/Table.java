55306bbc8db3782db2f942e8eb72883347371adb
Alexandr Kuzyaev <aworkaccs@gmail.com>
2018-03-29 00:19:56 +0500

    Table names

--- src/main/java/com/smartcity/smartHouse/db/Table.java
+++ src/main/java/com/smartcity/smartHouse/db/Table.java
@@ -4,4 +4,9 @@ public class Table {
     public static final String SM_USER = "SM_USER";
     public static final String SM_ADMIN = "SM_ADMIN";
     public static final String SM_INTEGR = "SM_INTEGR";
+    public static final String SM_HOUSE = "SM_HOUSE";
+    public static final String SM_ACTOR = "SM_ACTOR";
+    public static final String SM_SENSOR = "SM_SENSOR";
+    public static final String SM_SCENARIO = "SM_SCENARIO";
+    public static final String SM_EXTREME_SITUATION = "SM_EXTREME_SITUATION";
 }
